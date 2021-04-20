8ce19065446b00b32a13c06cc8cd9afa708d8d52
Lisa Pessin <lisapessin@gmail.com>
2021-04-20 10:10:50 -0400

    fixed prompts

--- index.js
+++ index.js
@@ -8,105 +8,145 @@ const Employee = require("./lib/employee.js");
 const Manager = require("./lib/manager.js");
 const Engineer = require("./lib/engineer.js");
 const Intern = require("./lib/intern.js");
-const generateHTML = require("./utils/generateHTML.js");
+// const generateHTML = require("./utils/generateHTML.js");
 
 
+let employeeArray = [];
 
-
-function promptQuestions(answers) {
+const promptQuestions = () => {
     inquirer.prompt([
         {
             type: 'input',
             message: "What is the team manager's name?",
-            name: 'name',
+            name: 'nameManager',
         },
         {
             type: 'input',
             message: "What is the team manager's employee ID?",
-            name: 'id',
+            name: 'idManager',
         },
         {
             type: 'input',
             message: "What is the team manager's email address?",
-            name: 'email',
+            name: 'emailManager',
         },
         {
             type: 'input',
             message: "What is the team manager's office number?",
-            name: 'officeNumber',
+            name: 'officeNumberManager',
         },
-
-    ]).then (function(newEmployee) {
-        inquirer.prompt([
-            {
-                type: 'list',
-                message: 'What type of employee would you like to add?',
-                choices: ['Engineer', 'Intern', "I don't want to add any more employees"],
-                name: 'addEmployee',
-            },
-        ]);
-    
-        if (newEmployee.addEmployee === 'Engineer') {
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    message: "What is the engineer's name?",
-                    name: 'name',
-                },
-                {
-                    type: 'input',
-                    message: "What is the engineer's employee ID?",
-                    name: 'id',
-                },
-                {
-                    type: 'input',
-                    message: "What is the engineer's email address?",
-                    name: 'email',
-                },
-                {
-                    type: 'input',
-                    message: "What is the engineer's GitHub username?",
-                    name: 'github',
-                },
-            ]);
-
-            newEmployee();
-
-        } else if (newEmployee.addEmployee === 'Intern') {
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    message: "What is the intern's name?",
-                    name: 'name',
-                },
-                {
-                    type: 'input',
-                    message: "What is the intern's employee ID?",
-                    name: 'id',
-                },
-                {
-                    type: 'input',
-                    message: "What is the intern's email address?",
-                    name: 'email',
-                },
-                {
-                    type: 'input',
-                    message: "What is the intern's school?",
-                    name: 'school',
-                },
-            ]);
-
-            newEmployee();
-
-        } else {
-            console.log("Game Over");
-        }
-    });
-
-
+        {
+            type: 'list',
+            message: 'What type of employee would you like to add?',
+            choices: ['Engineer', 'Intern', "I don't want to add any more employees"],
+            name: 'addEmployee',
+        },
+    ])
+    .then(answer => {
+        const manager = new Manager(answer.nameManager, answer.idManager, answer.emailManager, answer.officeNumberManager,);
+        employeeArray.push(manager);
         
-    
-}
-
+        if (answer.addEmployee === 'Engineer') {
+            engineerQuestions();
+        } else if (answer.addEmployee === 'Intern') {
+            internQuestions();
+        } else {
+            console.log('Game Over');
+            console.log(employeeArray);
+            return;
+        }
+    })
+};
 
 promptQuestions();
+
+const engineerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the engineer's name?",
+            name: 'nameEngineer',
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's employee ID?",
+            name: 'idEngineer',
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's email address?",
+            name: 'emailEngineer',
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's GitHub username?",
+            name: 'githubEngineer',
+        },
+        {
+            type: 'list',
+            message: 'What type of employee would you like to add?',
+            choices: ['Engineer', 'Intern', "I don't want to add any more employees"],
+            name: 'addEmployee',
+        },
+    ])
+    .then(answer => {
+        const engineer = new Engineer(answer.nameEngineer, answer.idEngineer, answer.emailEngineer, answer.githubEngineer);
+        employeeArray.push(engineer);
+
+        if (answer.addEmployee === 'Engineer') {
+            engineerQuestions();
+        } else if (answer.addEmployee === 'Intern') {
+            internQuestions();
+        } else {
+            console.log('Game Over');
+            console.log(employeeArray);
+            return;
+        }
+    })
+};
+
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the intern's name?",
+            name: 'nameIntern',
+        },
+        {
+            type: 'input',
+            message: "What is the intern's employee ID?",
+            name: 'idIntern',
+        },
+        {
+            type: 'input',
+            message: "What is the intern's email address?",
+            name: 'emailIntern',
+        },
+        {
+            type: 'input',
+            message: "What is the intern's school?",
+            name: 'schoolIntern',
+        },
+        {
+            type: 'list',
+            message: 'What type of employee would you like to add?',
+            choices: ['Engineer', 'Intern', "I don't want to add any more employees"],
+            name: 'addEmployee',
+        },
+    ])
+    .then(answer => {
+        const intern = new Intern(answer.nameIntern, answer.idIntern, answer.emailIntern, answer.schoolIntern);
+        employeeArray.push(intern);
+
+        if (answer.addEmployee === 'Engineer') {
+            engineerQuestions();
+        } else if (answer.addEmployee === 'Intern') {
+            internQuestions();
+        } else {
+            console.log('Game Over');
+            console.log(employeeArray);
+            return;
+        }
+    })
+};
+
